8cc7734b17fb4c6bcd3117a90e239e84547a94d6
Barney <asdf01618@gmail.com>
2023-12-01 22:23:28 +0800

    [add]調整menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -36,54 +36,75 @@
         </router-link>
       </div>
     </nav>
-    <Dialog
-      as="div"
-      class="lg:hidden"
-      :open="mobileMenuOpen"
-      @close="mobileMenuOpen = false"
+    <TransitionRoot
+      :show="mobileMenuOpen"
+      as="template"
     >
-      <div class="fixed inset-0 z-10" />
-      <DialogPanel
-        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+      <Dialog
+        as="div"
+        class="relative z-10 lg:hidden"
+        @close="mobileMenuOpen = false"
       >
-        <div class="flex items-center justify-between">
-          <a
-            href="#"
-            class="-m-1.5 p-1.5"
-          >
-            <span class="sr-only">Your Company</span>
-            <img
-              class="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
-            />
-          </a>
-          <button
-            type="button"
-            class="-m-2.5 rounded-md p-2.5 text-gray-700"
-            @click="mobileMenuOpen = false"
-          >
-            <span class="sr-only">Close menu</span>
-            Close
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-gray-500/10">
-            <div class="space-y-2 py-6">
-              <router-link
-                v-for="item in navigation"
-                :key="item.name"
-                :to="item.href"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                @click="mobileMenuOpen = false"
-              >
-                {{ item.name }}
-              </router-link>
-            </div>
+        <TransitionChild
+          enter="duration-300 ease-out"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="duration-200 ease-in"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div class="fixed inset-0 bg-black/30" />
+        </TransitionChild>
+
+        <div class="absolute inset-0 overflow-hidden">
+          <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            <TransitionChild
+              as="template"
+              enter="transform transition ease-in-out duration-500 sm:duration-700"
+              enter-from="translate-x-full"
+              enter-to="translate-x-0"
+              leave="transform transition ease-in-out duration-500 sm:duration-700"
+              leave-from="translate-x-0"
+              leave-to="translate-x-full"
+            >
+              <DialogPanel class="pointer-events-auto w-screen max-w-md">
+                <div class="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                  <div class="px-4 sm:px-6">
+                    <div class="flex items-start justify-between">
+                      <DialogTitle class="text-base font-semibold leading-6 text-gray-900">
+                        Panel title
+                      </DialogTitle>
+                      <div class="ml-3 flex h-7 items-center">
+                        <button
+                          type="button"
+                          class="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          @click="mobileMenuOpen = false"
+                        >
+                          <span class="absolute -inset-2.5" />
+                          <span class="sr-only">Close panel</span>
+                          Close
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="relative mt-6 flex-1 px-4 sm:px-6">
+                    <router-link
+                      v-for="item in navigation"
+                      :key="item.name"
+                      :to="item.href"
+                      class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      @click="mobileMenuOpen = false"
+                    >
+                      {{ item.name }}
+                    </router-link>
+                  </div>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
           </div>
         </div>
-      </DialogPanel>
-    </Dialog>
+      </Dialog>
+    </TransitionRoot>
   </header>
 </template>
 <script setup>
