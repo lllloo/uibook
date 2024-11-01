6f191be87b5433666116ec0eb44a650aefe47375
lllloo <asdf01618@gmail.com>
2024-11-01 01:20:10 +0000

    fix: codespace hot reload not stop

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,32 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm'
   ],
   framework: {
-    name: "@storybook/vue3-vite",
-    options: {},
+    name: '@storybook/vue3-vite',
+    options: {}
   },
   docs: {
     // 全域開啟自動文檔案
-    autodocs: true,
+    autodocs: true
   },
-};
-export default config;
+  // WSS 連接到 GitHub Codespace 失敗而導致重新載入循環
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    const { mergeConfig } = await import('vite')
+
+    return mergeConfig(config, {
+      server: {
+        hmr: {
+          clientPort: process.env.CODESPACES ? 443 : undefined // <== this is the magic
+        }
+      }
+    })
+  }
+}
+export default config
